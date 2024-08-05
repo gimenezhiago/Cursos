9ff50655c33f07c04644c1c6a6813a45683ca458
gimenezhiago <163415252+gimenezhiago@users.noreply.github.com>
2024-08-05 16:38:34 -0300

    hgf

--- Python/Exercicios/ConsumoMedio.py
+++
@@ -1,6 +0,0 @@
-combustivelGasto = eval(input("Quanto de combustível foi gasto na viagem (em Litros): "))
-kmPercorridos = eval(input("Qual foi a distância na viagem (Em Quilometros): "))
-
-consumoMedio = kmPercorridos/combustivelGasto
-
-print("O consumo médio foi {} km/l".format(consumoMedio))
