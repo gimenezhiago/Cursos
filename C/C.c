edfc6741339a6d7a5815abe3a56dc35a445ef544
gimenezhiago <163415252+gimenezhiago@users.noreply.github.com>
2024-08-25 10:14:18 -0300

    pljh

---
+++ C/C.c
@@ -0,0 +1,31 @@
+/*
+
+//Saída de dados 
+printf('oi')
+
+//Especificadores %
+d ou i = Z base decimal
+x = Z base hexadecimal
+f = Ponto com casas decimais
+e = Notação com casas decimais
+c = Texto letra
+s = Texto palavra
+.<num> = Quantos números dps virgula
+
+//Escape
+\n = Quebra linha
+\a = Som
+\b = Espaço
+\t = Tab
+\r = Inicio linha
+\0 = Nulo
+\v = Quebra coluna
+\\ = \
+\' = '
+\" = "
+\? = ?
+\123 = Octal
+\x12 = Hexadecimal
+%% = %
+
+*/
--- C/Comeco.c
+++ C/Comeco.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main() {
-    //Código
-    printf("Hello Word!!!\n");
-
-    system("pause");
+    printf("Valor inteiro: %d. \n", 10)
 }
