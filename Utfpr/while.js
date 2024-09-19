f8141424ebd34bcc60ae599bf31dd6b33d70343b
Hiago Gimenez <163415252+gimenezhiago@users.noreply.github.com>
2024-09-19 09:49:38 -0300

    kh

--- Utfpr/while.js
+++ Utfpr/while.js
@@ -5,5 +5,12 @@ function contar(n) {
         i++
     }
 }
-
 contar(5)
+
+function calcular(n) {
+    for (let i= 0; i<=10; i++) {
+        console.log(`${n} x ${i} = ${n * i}`)
+    }
+}
+
+calcular(7)
