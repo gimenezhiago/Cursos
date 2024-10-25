4de50621d0c4ecc9c9684f0f73d278f5d3551b1c
Hiago Gimenez <hiagogimenezvieira888@gmail.com>
2024-10-25 08:35:54 -0300

    gfgnhgfnhm

---
+++ Udemy/Aula 22/index.js
@@ -0,0 +1,4 @@
+let num1 = Number(window.prompt("Digite um número"))
+let num2 = Number(window.prompt("Digite outro número"))
+
+window.alert(`Os números são ${num1} e ${num2}, e a soma é ${num1 + num2}`)
