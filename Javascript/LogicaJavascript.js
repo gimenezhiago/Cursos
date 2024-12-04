e7667ccc4535fa65976ed8f2e462e29cb69bcc04
Hiago Gimenez <163415252+gimenezhiago@users.noreply.github.com>
2024-12-04 09:42:16 -0300

    dfrgthy

--- Javascript/LogicaJavascript.js
+++ Javascript/LogicaJavascript.js
@@ -7,3 +7,8 @@
 /* === Igualdade estrita(valor e tipo) */
 /* != Diferente(valor) */
 /* !== Diferente estrito(valor e tipo) */
+
+//Operadores lógicos
+/* && AND/E (Todas as expressões precisam ser verdadeiras) */ 
+/* || OR/OU (Não precisa todas serem verdadeiras) */
+/* ! NOT/NÃO (Inverte a expressão)*/  
