2d283c4613ba02c77b5378a73ed5a9f69e6e22b4
Hiago Gimenez <hiagogimenezvieira888@gmail.com>
2025-01-07 19:09:43 -0300

    cvbnmsrt

--- Javascript/FuncoesJavascript.js
+++ Javascript/FuncoesJavascript.js
@@ -22,3 +22,28 @@ const objeto = {
     }
 }
 objeto.falar()
+
+//Parâmetro, arguments
+function funcao() { //so funciona com o function
+    console.log(arguments[1]) //argumentos que sustenta todos os argumentos enviados
+} //mesmo com parâmetros, tem arguments
+funcao(1, 2, 3, 4, 'jfndf') //esse parâmetros n são precisos, ent eles viram arguments(array)
+
+//Fazer o parâmetro com valor definido caso não tenha
+function funcao1(a, b = 2, c = 3) {
+    return a + b + c
+}
+funcao1(2, undefined , 20) //como b ta no meio, tem q deixar undefined
+
+//Atribuição via desestruturação
+function funçao2 ({ nome, idade }) {
+    console.log(nome, idade)
+} 
+funçao2({ nome: 'Hiago', idade: 16 })
+
+function funçao3 ([ nome, idade ]) {
+    console.log(nome, idade)
+} 
+funçao2([ 'Hiago', 16 ])
+
+//
