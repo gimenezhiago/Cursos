f50dcc62c0413ce86550830e5b644306790cc702
gimenezhiago <163415252+gimenezhiago@users.noreply.github.com>
2025-09-01 23:50:34 -0300

    vcbn

--- Javascript/NpmJavascript.js
+++ Javascript/NpmJavascript.js
@@ -49,6 +49,44 @@ const path = require('path') //módulo nativo do Node.js para manipulação de c
 const caminhoArquivo = path.resolve(__dirname, 'pasta1', 'arquivo.js') //caminho absoluto Exemplo: /home/usuario/projeto/pasta1/arquivo.js
 const cami = path.resolve(__dirname, '..', 'arquivo.js') //caminho absoluto voltando uma pasta Exemplo: /home/usuario/arquivo.js
 
+//npm
+//npm init -y (inicializa um novo projeto Node.js com um package.json padrão)
+//npm install nome-do-pacote (instala um pacote e adiciona ao package.json)
+//npm install nome-do-pacote@0.0.0 (instala uma versão específica de um pacote)
+//npm install nome-do-pacote -E (instala um pacote e fixa a versão exata no package.json)
+//npm install nome-do-pacote --save-dev (instala um pacote como dependência de desenvolvimento)
+//npm update (atualiza todos os pacotes para a versão mais recente, respeitando as restrições do package.json)
+//npm uninstall nome-do-pacote (remove um pacote e atualiza o package.json)
+//npm list (lista todos os pacotes instalados no projeto)
+//npm list --depth=0 (lista apenas os pacotes de nível superior instalados no projeto)
+//npm outdated (verifica se há pacotes desatualizados no projeto)
 
+//package.json
+{
+    "name": "meu-projeto", //nome do projeto
+    "version": "1.0.0", //versão do projeto
+    "description": "Descrição do meu projeto", //descrição do projeto
+    "main": "index.js", //arquivo principal
+    "scripts": { //scripts para automatizar tarefas
+        "start": "node index.js",
+        "test": "echo \"Error: no test specified\" && exit 1"
+    },
+    "author": "Seu Nome", //autor do projeto
+    "license": "ISC", //licença do projeto
+    "dependencies": { //dependências do projet{
+    }
+    "devDependencies": { //dependências de desenvolvimento
+    }
+}
+
+//dependências (para instalar pacotes necessários para o projeto)
+//devDependencies (para instalar pacotes necessários apenas para desenvolvimento)
+
+// ^ (permite atualizações que não quebram compatibilidade, ou seja, atualizações de patch e minor
+// ~ (permite apenas atualizações de patch, ou seja, correções de bugs)
+// * (permite qualquer atualização, incluindo major, minor e patch)
+//major (atualizações que quebram compatibilidade) 
+//minor (atualizações com novas funcionalidades, mas compatíveis)
+//patch (atualizações de correção de bugs, sem novas funcionalidades)
 
 
