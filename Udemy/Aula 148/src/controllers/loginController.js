803f2e7d895c88ef127ea03fbc29bf2cf07d0150
gimenezhiago <hiagogimenezvieira888@gmail.com>
2026-01-02 11:36:32 -0300

    sdfgghh

--- Udemy/Aula 148/src/controllers/loginController.js
+++ Udemy/Aula 148/src/controllers/loginController.js
@@ -1,3 +1,26 @@
+const Login = require("../models/LoginModel");
+
 exports.index = (req, res) => {
-  res.render('login');
+  res.render("login");
+};
+
+exports.register = async function (req, res) {
+  try {
+    const login = new Login(req.body);
+    await login.register();
+
+    if (login.errors.length > 0) {
+      req.flash("errors", login.errors);
+      req.session.save(() => res.redirect("back"));
+      return;
+    }
+
+    req.flash("success", "Usuário cadastrado com sucesso.");
+    req.session.save(() => res.redirect("back"));
+
+    return res.send(req.body);
+  } catch (e) {
+    console.log(e);
+    return res.render("404");
+  }
 };
