623994dddce75fd4c0c2405813b744f1c5783ccf
gimenezhiago <hiagogimenezvieira888@gmail.com>
2026-01-19 12:28:17 -0300

    dvfbgnvhj

--- Udemy/Aula 148/routes.js
+++ Udemy/Aula 148/routes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const route = express.Router();
 const loginController = require('./src/controllers/loginController');
 const homeController = require('./src/controllers/homeController');
+const contatoController = require('./src/controllers/contatoController');
+
+const { loginRequired } = require('./src/middlewares/middleware');
+
 
 // Rotas da home
 route.get('/', homeController.index);
@@ -11,6 +15,11 @@ route.get('/login/index', loginController.index);
 route.post('/login/register', loginController.register);
 route.post('/login/login', loginController.login);
 route.get('/login/logout', loginController.logout);
+
+route.get('/contato/index', loginRequired, contatoController.index);
+route.post('/contato/register', loginRequired, contatoController.register);
+route.get('/contato/index/:id', loginRequired, contatoController.editIndex);
+
 
 
 
--- Udemy/Aula 148/src/middlewares/middleware.js
+++ Udemy/Aula 148/src/middlewares/middleware.js
@@ -20,3 +20,12 @@ exports.csrfMiddleware = (req, res, next) => {
   res.locals.csrfToken = req.csrfToken();
   next();
 };
+
+exports.loginRequired = (req, res, next) => {
+  if(!req.session.user) {
+    req.flash('errors', 'Você precisa fazer login para acessar essa página.');
+    req.session.save(() => res.redirect('/login/index'));
+    return;
+  }
+  next()
+}
