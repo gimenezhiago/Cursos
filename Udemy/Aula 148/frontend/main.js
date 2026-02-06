711db1ac8d026473bacf08f9aad48816d329ffd8
gimenezhiago <hiagogimenezvieira888@gmail.com>
2026-02-05 21:11:02 -0300

    xcvbn

--- Udemy/Aula 148/frontend/main.js
+++ Udemy/Aula 148/frontend/main.js
@@ -1,6 +1,9 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-import './assets/css/style.css';
+import Login from './modules/Login.js';
 
-console.log('Olá mundo 3');
+const cadastro = new Login('.form-cadastro');
+const login = new Login('.form-login');
+login.init();
+cadastro.init();
